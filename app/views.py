bf628c4b007b622455edd67277e4a2681431133a
yellojisubramanyam <yellojisubramanyam@gmail.com>
2023-08-01 10:26:16 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def urlnavigation1(request):
+    return render(request,'urlnavigation1.html')
+def urlnavigation2(request):
+    return render(request,'urlnavigation2.html')
